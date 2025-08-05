2a137056e70dca01212bea1f8c70741cd2e6d752
gogugogiugogu <o.piotrowski-08@o2.pl>
2025-08-05 07:58:48 +0100

    Add files via upload

---
+++ print.py
@@ -0,0 +1,20 @@
+car = "Tesla"
+model = " Model 3"
+print(car+model)
+
+#displays the number 10
+number = 1
+number = 10 
+print(number)
+
+a = "foot"
+b = "ball"
+print(a+b)
+
+print("Hello"+" there"+"!")
+
+#current date (took me a while to get this correctly LMAO)
+day = "5 "
+month = " august "
+year = "2025"
+print(month+day+year)
