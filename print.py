7bb436f203b5da712895d82216a828ee3b042fcb
gogugogiugogu <o.piotrowski-08@o2.pl>
2025-08-05 10:53:44 +0100

    print.py

--- print.py
+++ print.py
@@ -1,20 +1,24 @@
 car = "Tesla"
 model = " Model 3"
 print(car+model)
+#output = Tesla Model 3
 
-#displays the number 10
 number = 1
 number = 10 
 print(number)
+#output = 10
 
 a = "foot"
 b = "ball"
 print(a+b)
+#output = football
 
 print("Hello"+" there"+"!")
+#output = Hello there!
 
 #current date (took me a while to get this correctly LMAO)
 day = "5 "
 month = " august "
 year = "2025"
 print(month+day+year)
+#output = august 5 2025
